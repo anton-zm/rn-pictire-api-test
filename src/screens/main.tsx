56b7b7438ba1e5991158eb415c4b7cd6b116e3cd
anton-zm <antonzm34@gmail.com>
2021-12-22 11:08:18 +0300

    remove default app tsx

--- src/screens/main.tsx
+++ src/screens/main.tsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import { View, Text } from 'react-native'
 
 export const MainScreen = () => {
     return (
-        <></>
+        <>
+        <View>
+            <Text>Hello world!</Text>
+        </View>
+        </>
     )
 }
