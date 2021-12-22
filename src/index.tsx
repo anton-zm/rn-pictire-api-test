15b8aafd658e7117dd33b4c2931eb8d9a0c12837
anton-zm <antonzm34@gmail.com>
2021-12-22 10:58:21 +0300

    add project structure

---
+++ src/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack'
+import { Intro } from './screens/intro';
+import { MainScreen } from './screens/main';
+import { LoginScreen } from './screens/login';
+
+const Route = createStackNavigator();
+
+export default function App(){
+  
+  return (
+    <NavigationContainer>
+        <Route.Navigator screenOptions={{headerShown:false}}>
+            <Route.Screen name='Intro' component={Intro} />
+            <Route.Screen name='Main' component={MainScreen} />
+        </Route.Navigator>
+    </NavigationContainer>
+  );
+};
